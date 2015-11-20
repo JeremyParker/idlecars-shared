174e2325028be19cdbd6f41e90bc94f6ce82e995
craigstar <craig@idlecars.com>
2015-11-20 16:21:29 -0500

    navbar init

--- components/single_choice/directive.js
+++ components/single_choice/directive.js
@@ -5,13 +5,15 @@ angular.module('idlecars')
   return {
     templateUrl: 'shared/components/single_choice/template.html',
     controller: function ($scope) {
-      $scope.selectedIndex = 0;
-      $scope.selectedItem = $scope.choices[$scope.selectedIndex];
+      if ($scope.choices) {
+        $scope.selectedIndex = 0;
+        $scope.selectedItem = $scope.choices[$scope.selectedIndex];
 
-      $scope.click = function (index) {
-        $scope.selectedIndex = index;
-        $scope.selectedItem = $scope.choices[index];
-      }
+        $scope.click = function (index) {
+          $scope.selectedIndex = index;
+          $scope.selectedItem = $scope.choices[index];
+        }
+      };
     },
     scope: {
       choices: '=',
