e3cf31417d6c39a5414ef24dd25e49178decb939
craigstar <craig@idlecars.com>
2015-11-20 15:10:10 -0500

    selected index and selected item

--- components/single_choice/directive.js
+++ components/single_choice/directive.js
@@ -4,16 +4,18 @@ angular.module('idlecars')
 .directive('singleChoice', function () {
   return {
     templateUrl: 'shared/components/single_choice/template.html',
-    controller: function ($scope, $timeout) {
-      $scope.select = 0;
+    controller: function ($scope) {
+      $scope.selectedIndex = 0;
+      $scope.selectedItem = $scope.choices[$scope.selectedIndex];
 
       $scope.click = function (index) {
-        $scope.select = index;
+        $scope.selectedIndex = index;
+        $scope.selectedItem = $scope.choices[index];
       }
     },
     scope: {
       choices: '=',
-      select: '=',
+      selectedItem: '=',
     }
   };
 });
