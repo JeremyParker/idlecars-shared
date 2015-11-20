fb86ba32d11ce827e7f425dfb3b2d7c48d69fd18
craigstar <craig@idlecars.com>
2015-11-20 14:33:38 -0500

    get rid of mock up

--- components/single_choice/directive.js
+++ components/single_choice/directive.js
@@ -4,14 +4,16 @@ angular.module('idlecars')
 .directive('singleChoice', function () {
   return {
     templateUrl: 'shared/components/single_choice/template.html',
-    controller: function ($scope) {
+    controller: function ($scope, $timeout) {
       $scope.select = 0;
+
       $scope.click = function (index) {
         $scope.select = index;
       }
     },
     scope: {
       choices: '=',
+      select: '=',
     }
   };
 });
