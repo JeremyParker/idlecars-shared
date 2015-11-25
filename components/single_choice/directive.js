0f74e0e936becdad54f99c5999144c7496f9fcf7
craigstar <craig@idlecars.com>
2015-11-25 16:14:48 -0500

    auto init scroll to the choice

--- components/single_choice/directive.js
+++ components/single_choice/directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('idlecars')
-.directive('singleChoice', function () {
+.directive('singleChoice', function ($interval) {
   return {
     templateUrl: 'shared/components/single_choice/template.html',
     controller: function ($scope) {
@@ -9,6 +9,9 @@ angular.module('idlecars')
         // the basic height of each line is 48px and border is 2px
         $scope.maxHeight = 48 * $scope.maxHeight + 2;
       };
+
+      // this flag means we don't auto scroll after user's click
+      var scrollInit = true;
 
       var getIndex = function () {
         var choices = $scope.choices;
@@ -26,14 +29,42 @@ angular.module('idlecars')
         if ($scope.choices) {
           $scope.selectedIndex = getIndex();
           $scope.ngModel = $scope.choices[$scope.selectedIndex].key;
+
+          if (scrollInit) {
+            $scope.scrollTo($scope.selectedIndex)
+          }
         };
       }
 
       $scope.$watch('ngModel', loadModel);
 
       $scope.click = function (index) {
+        scrollInit = false;
+
         $scope.selectedIndex = index;
         $scope.ngModel = $scope.choices[index].key;
+      }
+    },
+    link: function (scope, element, attrs) {
+      scope.scrollTo = function (index) {
+        var scrollHeight = element[0].children[0].scrollHeight;
+        var clientHeight = element[0].children[0].clientHeight;
+        var maxScrollPosition = scrollHeight - clientHeight;
+
+        var desiredPosition = index * 48;
+
+        if (desiredPosition > maxScrollPosition) {
+          desiredPosition = maxScrollPosition;
+        };
+
+        // try to scroll to the desired position until its done
+        var scrollToPosition = $interval(function () {
+          element[0].children[0].scrollTop = desiredPosition;
+
+          if (element[0].children[0].scrollTop >= desiredPosition) {
+            $interval.cancel(scrollToPosition);
+          };
+        }, 1)
       }
     },
     scope: {
