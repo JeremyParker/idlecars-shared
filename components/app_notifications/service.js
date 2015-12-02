8a98af675e28a46bb8603d0dfe73675082d30865
craigstar <craig@idlecars.com>
2015-12-02 16:58:52 -0500

    timeout function

--- components/app_notifications/service.js
+++ components/app_notifications/service.js
@@ -8,12 +8,12 @@ angular.module('idlecars')
     service.messages.push(message);
 
     // notification goes off automatically after 15s
-    $timeout(function () { service.remove(message) }, 15000);
+    $timeout(function () { service.remove() }, 15000);
   }
 
-  service.remove = function(message) {
-    var index = service.messages.lastIndexOf(message);
-    if (index > -1) { service.messages.splice(index, 1) }
+  service.remove = function(index) {
+    if (index) { service.messages.splice(index, 1) }
+    else { service.messages.pop() }
   }
 
   return service;
