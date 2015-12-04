153ebfd32e6f6ed285c7d88b3fe278a59b6b5068
craigstar <craig@idlecars.com>
2015-12-04 15:44:37 -0500

    accept notifications from successful response

--- components/app_notifications/interceptor.js
+++ components/app_notifications/interceptor.js
@@ -4,13 +4,21 @@ angular.module('idlecars')
 .factory('appNotificationInterceptor', function($q, AppNotificationService) {
   var interceptor = {};
 
-  interceptor.responseError = function(rejection) {
-    var notifications = _notifications(rejection);
+  var pushNotifications = function (result) {
+    var notifications = _notifications(result);
     for (var i = 0; i < notifications.length; i++) {
       AppNotificationService.push(notifications[i]);
     }
+  }
 
+  interceptor.responseError = function(rejection) {
+    pushNotifications(rejection);
     return $q.reject(rejection);
+  }
+
+  interceptor.response = function (response) {
+    pushNotifications(response);
+    return response;
   }
 
   var _notifications = function(rejection) {
